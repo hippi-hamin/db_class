211bec202a600cd21b3624e9fc0686d466454e11
hippi-hamin <9908haha@naver.com>
2024-01-04 13:51:01 +0900

    조회연습

--- DB수업_foreign key.sql
+++ DB수업_foreign key.sql
@@ -168,3 +168,32 @@ insert into orders(customer_id, book_id, o_saleprice, o_orderdate) values(3, 10,
 insert into orders(customer_id, book_id, o_saleprice, o_orderdate) values(2, 10, 7000, str_to_date('2023-07-09','%Y-%m-%d'));
 insert into orders(customer_id, book_id, o_saleprice, o_orderdate) values(3, 8, 13000, str_to_date('2023-07-10','%Y-%m-%d'));
 select * from orders;
+
+-- 조회 연습
+-- 1. 모든 도서의 가격과 도서명 조회
+select b_price, b_bookname from book;
+-- 2. 모든 출판사 이름 조회 
+select b_publisher from book;
+-- 2.1 중복값을 제외한 출판사 이름 조회
+select distinct b_publisher from book;
+-- 3. BOOK테이블의 모든 내용 조회 
+select * from book;
+-- 4. 20000원 미만의 도서만 조회 
+select * from book where b_price < 20000;
+-- 5. 10000원 이상 20000원 이하인 도서만 조회
+select * from book where b_price <= 20000 and b_price >= 10000;
+select * from book where b_price between 10000 and 20000; -- (between/and = 이상 이하의 개념이라 초과 미만일 땐 참고)
+-- 6. 출판사가 좋은출판사 또는 대한출판사인 도서 조회 
+select * from book where b_publisher = '좋은출판사' or b_publisher = '대한출판사';
+select * from book where b_publisher in('좋은출판사', '대한출판사');
+-- 7. 도서명에 축구가 포함된 모든 도서를 조회
+select * from book where b_bookname like '%축구%';
+-- 8. 도서명의 두번째 글자가 구인 도서 조회
+select * from book where b_bookname like '_구%';
+-- 9. 축구 관련 도서 중 가격이 20000원 이상인 도서 조회
+select * from book where b_bookname like '%축구%' and b_price > 20000;
+-- 10. 책 이름순으로 전체 도서 조회
+select * from book order by b_bookname asc;
+select * from book order by b_bookname desc;
+-- 11. 도서를 가격이 낮은 것 부터 조회하고 같은 가격일 경우 도서명을 가나다 순으로 조회
+select * from book order by b_price asc, b_bookname asc;
