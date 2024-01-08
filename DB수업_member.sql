8e4be744bc004dbb3455ab8f9b921043ac69f0b8
hippi-hamin <9908haha@naver.com>
2024-01-09 08:56:52 +0900

    회원기능

--- DB수업_member.sql
+++ DB수업_member.sql
@@ -59,3 +59,28 @@ create table good_table(
     constraint fk_good_comment foreign key(comment_id) references comment_table(id) on delete cascade,
     constraint fk_good_member foreign key(member_id) references member_table(id) on delete cascade
 );
+
+-- 회원 기능
+-- 1. 회원가입(임의의 회원3명 가입)
+insert into member_table(member_email, member_name, member_password)
+	values('aaa@member.com', 'aaa', '1234');
+insert into member_table(member_email, member_name, member_password)
+	values('bbb@member.com', 'ccc', '1111');    
+insert into member_table(member_email, member_name, member_password)
+	values('ccc@member.com', 'ccc', '1234');
+-- 2. 이메일 중복체크 
+select member_email from member_table where member_email='ddd@member.com';
+-- 3. 로그인
+select * from member_table where member_email='aaa@member.com' and member_password='1234';
+-- 4. 전체 회원 목록 조회 
+select * from member_table;
+-- 5. 특정 회원만 조회 
+select * from member_table where member_email='aaa@member.com';
+select * from member_table where id=1;
+-- 6. 내정보 수정하기(6.1, 6.2에 해당하는 쿼리문작성)
+-- 6.1 회원정보 수정화면 요청(회원정보 수정 페이지를 보여준다고 가정했을 때 필요한 쿼리) 
+select * from member_table where id=1;
+-- 6.2 회원정보 수정 처리(비밀번호를 변경한다는 상황)
+update member_table set member_password='0000' where id=1;
+-- 7. 회원 삭제 또는 탈퇴 
+delete from member_table where id=3;
